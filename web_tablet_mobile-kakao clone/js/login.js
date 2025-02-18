19ea8dba9a9f648beb531237ad6bcd8b33bd99c3
KimJinhee72 <geenhankuk@gmail.com>
2025-02-19 01:27:06 +0900

    Add files via upload
    final2

--- web_tablet_mobile-kakao clone/js/login.js
+++ web_tablet_mobile-kakao clone/js/login.js
@@ -1,40 +1,38 @@
-$(function () {  
-         
-    $(".helpp").hide();  
+$(function () {
+
+    $(".helpp").hide();
     // $("#image")
-    //   .mouseover(function () {        
-    //     $(".helpp").show(); 
+    //   .mouseover(function () {
+    //     $(".helpp").show();
     //     $(".help a").show()
     //   }).mouseout(function () {
     //     $(".helpp").hide();
-    //     $(".help a").hide();      
+    //     $(".help a").hide();
     //   })
     $("#image")
-      .mouseover(function () {        
-        $(".helpp").stop().show(); 
+      .mouseover(function () {
+        $(".helpp").stop().show();
         $(".help a").stop().show();
       })
       $("#image , .helpp").click(function () {
         $(".helpp").hide();
-        $(".help a").hide();      
+        $(".help a").hide();
       })
 
       // $("#image").click(function(){
-      //   $(".helpp").stop().show(); 
+      //   $(".helpp").stop().show();
       //   $(".help a").stop().show()
       // }).mouseout(function () {
       //       $(".helpp").hide();
-      //       $(".help a").hide();      
+      //       $(".help a").hide();
       //     })
 
       //   $(".help a").mouseover(function(){
       //       $(this).css("font-size", "17px");
-      //       $(this).css("cursor", "pointer");                             
+      //       $(this).css("cursor", "pointer");
       //   }).mouseout(function () {
       //       $(this).css("font-size", "16px");
-      //   })     
+      //   })
       // })
-      
+
   });
-  
-  
